b1e789c88e5c27feeb721848f5a038de3aacda0e
kbeinoras <kbeinoras@users.noreply.github.com>
2019-11-11 14:06:46 +0200

    Add project files.

---
+++ Items.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace antroji_praktika
+{
+    /// <summary>
+    /// Interaction logic for Items.xaml
+    /// </summary>
+    public partial class Items : UserControl
+    {
+        public Items()
+        {
+            InitializeComponent();
+        }
+        private string Title;
+        
+        public void setTitle(string TITLE)
+        {
+            Title = TITLE;
+            LblText.Content = Title;
+        }
+        public string GetTitle()
+        {
+            return Title;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SQLite;
+
+namespace antroji_praktika
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ShowData();
+        }
+     
+        public void ShowData()
+        {
+            SQLiteConnection m_dbConnection = new SQLiteConnection(@"Data Source=DbAntras.db;");
+            
+            m_dbConnection.Open();
+            string sql1 = $"SELECT Kategorija FROM Prekes";
+            SQLiteCommand command1 = new SQLiteCommand(sql1, m_dbConnection);
+            SQLiteDataReader reader1 = command1.ExecuteReader();
+            string name = "";
+            int k=0;
+            List<Items> Sarasas = new List<Items>();
+            while (reader1.Read())
+            {
+                name = reader1.GetString(0);
+                Sarasas.Add(new Items());
+                Sarasas[k].setTitle(name);
+                StackPanel1.Children.Add(Sarasas[k]);
+                k++;
+
+            }
+            m_dbConnection.Close();
+        }
+
+    }
+}
